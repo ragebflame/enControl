30cbc840149ffabdf07304aaa50ac231498b7a69
Daniel Flannery <ragebflame@gmail.com>
2018-10-18 13:36:15 +0000

    Adding pytest in order to unit test the flask api correctly

--- enControl-test.py
+++
@@ -1,9 +0,0 @@
-# Replace libraries by fake ones
-import sys
-import fake_rpi
-
-sys.modules['RPi'] = fake_rpi.RPi     # Fake RPi (GPIO)
-sys.modules['RPi.GPIO'] = fake_rpi.RPi.GPIO # Fake RPi (GPIO)
-# Call the script
-import enControl
-enControl.app.run(host = "0.0.0.0", port = 80, debug=True)
