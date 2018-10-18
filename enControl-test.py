36deff4357979e9d8b6085c1d0a22c6092d53b1e
Daniel Flannery <ragebflame@gmail.com>
2018-10-18 12:48:59 +0000

    Added sudo requirement in order to start the flask server

--- enControl-test.py
+++ enControl-test.py
@@ -6,4 +6,4 @@ sys.modules['RPi'] = fake_rpi.RPi     # Fake RPi (GPIO)
 sys.modules['RPi.GPIO'] = fake_rpi.RPi.GPIO # Fake RPi (GPIO)
 # Call the script
 import enControl
-enControl.app.run(host = "0.0.0.0", port = 80)
+enControl.app.run(host = "0.0.0.0", port = 80, debug=True)
