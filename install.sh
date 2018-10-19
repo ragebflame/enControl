e06f58b4c57b7013d088b255d89b950d707be779
Daniel Flannery <ragebflame@gmail.com>
2018-10-19 12:10:46 +0000

    Adding an auto install script

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "
+             ____            _             _               
+  ___ _ __  / ___|___  _ __ | |_ _ __ ___ | |  _ __  _   _ 
+ / _ \ '_ \| |   / _ \| '_ \| __| '__/ _ \| | | '_ \| | | |
+|  __/ | | | |__| (_) | | | | |_| | | (_) | |_| |_) | |_| |
+ \___|_| |_|\____\___/|_| |_|\__|_|  \___/|_(_) .__/ \__, |
+            Install script                    |_|    |___/ 
+"
+
+debian_install() {
+    sudo apt update && sudo apt upgrade -y
+    sudo apt install git python pip
+}
+
+python_setup() {
+    git clone https://github.com/ragebflame/enControl
+    cd enControl && sudo pip install -r requirements.txt
+
+    echo  "Installation complete!"
+}
+
+if [ "$(grep -Ei 'debian|ubuntu|mint' /etc/*release)" ]; then
+    debian_install
+    python_setup
+else
+    echo "Only Debian based distros are supported at the moment."
+fi
