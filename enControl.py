e9364d18b67e9f20e56b7d614bed60d8b6a13ffa
Daniel <ragebflame@gmail.com>
2019-05-31 14:20:34 +0100

    Added mock code for Redis implementation. This needs testing on the Pi itself.

--- enControl.py
+++ enControl.py
@@ -1,6 +1,9 @@
 #!flask/bin/python
 from flask import Flask, jsonify, abort, make_response, request
-import RPi.GPIO as GPIO
+from redis import Redis
+from rq import Queue, Worker, Connection
+from switch_control import trigger_switch
+#import RPi.GPIO as GPIO
 import time
 
 app = Flask(__name__)
@@ -40,6 +43,11 @@ gpio_values = {
         }]
 }
 
+# Define the task Queue
+request_queue = Queue(connection=Redis())
+# Define a worker
+worker = Worker(request_queue, connection=Redis())
+worker.work()
 
 # Return the gpio_pins dictionary
 @app.route('/energenie-control/api/v1.0/gpio_pins', methods=['GET'])
@@ -82,7 +90,11 @@ def post_command():
     print(data_mapping)
 
     # Send command to the switch
-    trigger_switch(data_mapping)
+    #trigger_switch(data_mapping, parsed_switch_id)
+
+    # Enqueue the function call
+    #result = request_queue.Enqueue(trigger_switch, data_mapping, parsed_switch_id)
+    request_queue.enqueue(trigger_switch, data_mapping, parsed_switch_id)
 
     # Return an OK
     return jsonify(request.json), 202
@@ -95,31 +107,6 @@ def not_found(error):
         'error': 'Not found'
         }), 404)
 
-
-# Function that sends the command to the pins
-def trigger_switch(data_mapping):
-    GPIO.setmode(GPIO.BOARD)  # set the pins numbering mode
-
-    # Select each of the GPIO pins needed
-    needed_pins = [11, 15, 16, 13, 18, 22]
-    data_pins = [11, 15, 16, 13]
-    GPIO.setup(needed_pins, GPIO.OUT)  # Default all pin values to lo / 0
-    GPIO.output(needed_pins, False)
-    try:  # Loop to map all GPIO.outputs# Set K0 - K3(aka D0 - D3)
-        for i in range(0, len(data_pins)):
-            GPIO.output(data_pins[i], data_mapping[i])
-
-        time.sleep(0.25)  # let it settle, encoder requires this
-        GPIO.output(22, True)  # Enable the modulator
-        time.sleep(0.25)  # keep enabled for a period
-        GPIO.output(22, False)  # Disable the modulator
-    except Exception:
-        print("Error trying to send the command. Exiting.")
-        abort(500)
-
-    # Clean up the GPIOs for next time
-    GPIO.cleanup()
-    print("Command sent sucessfully!")
 
 
 if __name__ == '__main__':
