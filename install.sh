ffc422554680e520ffb143dbeb0572c75c636adb
Daniel Flannery <ragebflame@gmail.com>
2019-09-18 21:13:37 +0100

    Linted the install.sh with Shellcheck

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "
-             ____            _             _               
-  ___ _ __  / ___|___  _ __ | |_ _ __ ___ | |  _ __  _   _ 
+             ____            _             _
+  ___ _ __  / ___|___  _ __ | |_ _ __ ___ | |  _ __  _   _
  / _ \ '_ \| |   / _ \| '_ \| __| '__/ _ \| | | '_ \| | | |
 |  __/ | | | |__| (_) | | | | |_| | | (_) | |_| |_) | |_| |
  \___|_| |_|\____\___/|_| |_|\__|_|  \___/|_(_) .__/ \__, |
-            Install script                    |_|    |___/ 
+            Install script                    |_|    |___/
 "
 
 debian_install() {
@@ -21,7 +21,8 @@ python_setup() {
     echo  "Installation complete!"
 }
 
-if [ "$(grep -Ei 'debian|ubuntu|mint' /etc/*release)" ]; then
+if grep -qEi 'debian|ubuntu|mint' /etc/*release
+ then
     debian_install
     python_setup
 else
